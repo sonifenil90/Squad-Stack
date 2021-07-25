e5a53d99a703088396430f2a1714de6d2d4cdf34
sonifenil90 <sonifenil90@gmail.com>
2021-07-25 18:42:46 +0530

    Create vehicle.py

---
+++ Activity Round/src/vehicle.py
@@ -0,0 +1,5 @@
+class Vechicle_Info:
+    ''' Constructor to initialize the vehicle object with the info given in input '''
+    def __init__(self, vehicle_reg_no, driver_age):
+        self.vehicle_reg_no = vehicle_reg_no
+        self.driver_age = driver_age
